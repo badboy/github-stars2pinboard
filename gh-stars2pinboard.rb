13279a87841c81876f4e208bce9dbb968137e66c
Jan-Erik Rediger <badboy@archlinux.us>
2013-01-17 18:47:19 +0100

    Cache starred repos, so we do not stress the pinboard API with already submitted content.

--- gh-stars2pinboard.rb
+++ gh-stars2pinboard.rb
@@ -8,7 +8,9 @@
 
 require 'octokit'
 require 'pinboard'
+require 'digest/sha1'
 
+CACHE_FILE = "./cache"
 Octokit.netrc = true
 
 pinclient = Pinboard::Client.new token: ARGV[0]
@@ -16,19 +18,42 @@ pinclient = Pinboard::Client.new token: ARGV[0]
 octo = Octokit::Client.new auto_traversal: true
 starred = octo.starred
 
+def hash name, lang, url
+  Digest::SHA1.hexdigest "%s:%s:%s" % [name, lang, url]
+end
+
+cache = []
+
+if File.exist?(CACHE_FILE)
+  cache = File.readlines(CACHE_FILE).map(&:chomp)
+end
+
+cache_file = File.open(CACHE_FILE, "w")
+i = 1
+total = starred.size
 starred.each do |star|
   name = star["name"]
   lang = (star["language"]||'').downcase
   desc = star["description"]
   url  = star["html_url"]
 
-  status = pinclient.add_post({
-    replace:     "no",
-    url:         url,
-    description: name,
-    extended:    desc,
-    tags:        "githubstars #{lang}"
-  })
+  print "#{i.to_s.rjust(total.to_s.size)}/#{total} #{name}: "
 
-  puts "#{name}: #{status}"
+  h = hash name, lang, url
+  cache_file.puts h
+  if cache.include?(h)
+    status = "skipped"
+  else
+    status = pinclient.add_post({
+      replace:     "no",
+      url:         url,
+      description: name,
+      extended:    desc,
+      tags:        "githubstars #{lang}"
+    })
+  end
+
+  puts status
+  i += 1
 end
+cache_file.close
